c9ea5436a21ec0700012dc28374368a509e5c174
Rinaldo <rturconi@mail.com>
2024-07-17 02:27:23 -0400

    se agrega estetica para scroll de navbar con jquery y se modifica el resto del código acorde

--- assets/js/scritp.js
+++ assets/js/scritp.js
@@ -2,3 +2,14 @@
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
 // SCRIPT PARA POPPER //
+
+
+
+
+$(window).scroll(function () {
+    var target = $('#start')[0].getBoundingClientRect();
+    if (target.bottom - target.height <= 100)
+        $(".navbg").css({ "background-color": "aqua", "border-bottom-left-radius": "20px", "border-bottom-right-radius": "20px", "transition": ".5s"})
+    if (target.bottom - target.height >= 100)
+        $(".navbg").css({ "background-color": "transparent", "transition": ".5s"});
+});

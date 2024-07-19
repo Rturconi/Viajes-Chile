35ad880f2bcb68b69031326cc6a9f077cbc796cd
Rinaldo <rturconi@mail.com>
2024-07-19 18:14:42 -0400

    se ajustan ultimos detalles de colores y se deja carrusel con movimiento automatico

--- assets/js/scritp.js
+++ assets/js/scritp.js
@@ -9,7 +9,7 @@ const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstra
 $(window).scroll(function () {
     var target = $('#start')[0].getBoundingClientRect();
     if (target.bottom - target.height <= 100)
-        $(".navbg").css({ "background-color": "aqua", "border-bottom-left-radius": "20px", "border-bottom-right-radius": "20px", "transition": ".5s"})
+        $(".navbg").css({ "background-color": "#08a5a5", "border-bottom-left-radius": "20px", "border-bottom-right-radius": "20px", "transition": ".5s"})
     if (target.bottom - target.height >= 100)
         $(".navbg").css({ "background-color": "transparent", "transition": ".5s"});
 });
